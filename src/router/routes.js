9bc7918ad389532ab86a08a01578256e5ac4ff39
itsemelona <62753224+em1730@users.noreply.github.com>
2023-07-18 11:24:34 +0800

    a

---
+++ src/router/routes.js
@@ -0,0 +1,25 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Admin/dashboardPage.vue') },
+      { path: '/administration', component: () => import('pages/Admin/administrationPage.vue') },
+      { path: '/module', component: () => import('pages/Admin/modulePage.vue') },
+      { path: '/endUser', component: () => import('pages/Admin/endUserPage.vue') },
+      { path: '/userLogs', component: () => import('pages/Admin/userLogsPage.vue')},
+      { path: '/masterList', component: () => import('pages/Admin/masterListPage.vue')}
+
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
