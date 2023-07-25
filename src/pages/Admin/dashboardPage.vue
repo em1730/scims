1d0ea9fbbe723ca301535e3d33df6441cda36e87
Jerhod Kyan A. Ricabo <jerhodkyan@gmail.com>
2023-07-25 19:58:55 +0800

    adding tailwindcss assets

--- src/pages/Admin/dashboardPage.vue
+++ src/pages/Admin/dashboardPage.vue
@@ -1,16 +1,91 @@
 <template>
   <q-page padding>
-    <div class="text-h5 text-center">Dashboard</div>
+    <div class="pt-20 grid place-items-center">
+      <div class="pb-5">
+        <img
+          class="h-48 w-48 rounded-full border drop-shadow-2xl"
+          src="https://vamosmobile.app/sccdrrmo/flutter/images/647acfbcc8828.jpg"
+          alt=""
+        />
+      </div>
+      <div class="pb-10">
+        <p class="text-center text-3xl">Jerhod Kyan A. Ricabo</p>
+      </div>
 
-    <div class="absolute-center">
-      <q-btn
-        flat
-        round
-        icon="admin_panel_settings"
-        size="4em"
-        color="blue-grey-10"
-        to="/administration"
-      />
+      <div class="flex flex-wrap vertical:flex-col justify-center items-center">
+        <div>
+          <!-- Doctrack System -->
+          <q-btn
+            dense
+            style="color: white"
+            unelevated
+            class="hover:scale-110 duration-300 hover:"
+            to="/administration"
+          >
+            <img
+              class="w-36 md:w-32 lg:w-42 sm:w-16"
+              src="https://vamosmobile.app/scims-v2/dashboard/images/webp/document_tracking.webp"
+            />
+          </q-btn>
+          <!-- End Doctrack System -->
+          <!-- Doctrack System -->
+          <q-btn
+            dense
+            style="color: white"
+            unelevated
+            class="hover:scale-110 duration-300 hover:"
+            type="submit"
+          >
+            <img
+              class="w-36 md:w-32 lg:w-42 sm:w-16"
+              src="https://vamosmobile.app/scims-v2/dashboard/images/webp/legislative.webp"
+            />
+          </q-btn>
+          <!-- End Doctrack System -->
+          <!-- Doctrack System -->
+          <q-btn
+            dense
+            style="color: white"
+            unelevated
+            class="hover:scale-110 duration-300 hover:"
+            type="submit"
+          >
+            <img
+              class="w-36 md:w-32 lg:w-42 sm:w-16"
+              src="https://vamosmobile.app/scims-v2/dashboard/images/webp/jomis.webp"
+            />
+          </q-btn>
+          <!-- End Doctrack System -->
+          <!-- Doctrack System -->
+          <q-btn
+            dense
+            style="color: white"
+            unelevated
+            class="hover:scale-110 duration-300 hover:"
+            type="submit"
+          >
+            <img
+              class="w-36 md:w-32 lg:w-42 sm:w-16"
+              src="https://vamosmobile.app/scims-v2/dashboard/images/webp/document_tracking.webp"
+            />
+          </q-btn>
+          <!-- End Doctrack System -->
+          <!-- Doctrack System -->
+          <q-btn
+            dense
+            style="color: white"
+            unelevated
+            class="hover:scale-110 duration-300 hover:"
+            type="submit"
+          >
+            <img
+              class="w-36 md:w-32 lg:w-42 sm:w-16"
+              src="https://vamosmobile.app/scims-v2/dashboard/images/webp/document_tracking.webp"
+            />
+          </q-btn>
+          <!-- End Doctrack System -->
+        </div>
+      </div>
     </div>
   </q-page>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./src/**/*.{vue,html,js}"],
   theme: {
     extend: {},
