55692833d20d86ea708e09e5b724e640b11ae40b
itsemelona <62753224+em1730@users.noreply.github.com>
2024-03-01 11:20:00 +0800

    Create FooterLayout.vue

---
+++ src/layouts/FooterLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-footer elevated class="bg-green-6 text-white">
+    <q-toolbar>
+      <q-toolbar-title>
+        <div class="text-left text-subtitle1">
+          Copyright @ 2024 | ITCSO |
+          <a href="http://sancarloscity.gov.ph/"> LGU San Carlos City</a><br />
+          All right reserved.
+        </div>
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-footer>
+</template>
+
+<script setup></script>
